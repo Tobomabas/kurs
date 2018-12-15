ef58259eba41299079729a3d61334ecafd793776
Tomasz <tobomabas@gmail.com>
2018-12-15 14:01:39 +0100

    Merge branches 'master' and 'tomek' of https://github.com/Tobomabas/kurs
    # Conflicts:
    #	src/main/java/pl/com/tkarolczyk/anonymous_classes/null_avoid/football/Main.java

--- src/main/java/pl/com/tkarolczyk/anonymous_classes/lambda/excercice/Main.java
+++ src/main/java/pl/com/tkarolczyk/anonymous_classes/lambda/excercice/Main.java
@@ -1,4 +1,45 @@
 package pl.com.tkarolczyk.anonymous_classes.lambda.excercice;
 
 public class Main {
+String jakisinny;
+
+    public static void main(String[] args) {
+
+        Trainer trainer = new Trainer();
+        MyThread myThread = new MyThread();
+
+
+
+        Excercice job = new Excercice() {
+            @Override
+            public void swim() {
+                System.out.println("Swimming !!");
+            }
+        };
+        Excercice lambdaJob = () -> {};  // void swim
+        MyRunnable run = () -> {};       // void run
+
+
+        trainer.performExcercice(() -> System.out.println("Swim"));
+        trainer.performExcercice(lambdaJob);
+
+        myThread.prformanceJob(() -> System.out.println("run"));
+        myThread.prformanceJob(run);
+    }
+    private static Excercice makeNewExcercise (){
+
+        return new Excercice() {
+            @Override
+            public void swim() {
+                System.out.println("My new Excercise!!");
+            }
+        };
+    }
+//
+//    private Excercice makeNewExcerciseInLabda(){
+//
+//        return () -> {this.jakisinny};
+//
+//
+//
 }
